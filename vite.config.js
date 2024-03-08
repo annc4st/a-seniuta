eb7a65ddcde1c0c663204bf8b0f32eaf262deb2b
annc4st <annaseniuta@gmail.com>
2024-03-08 15:02:40 +0000

    yml

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: '/a-seniuta/',
-  base: process.env.VITE_BASE || '/',
+  base: '/a-seniuta/',
+  // base: process.env.VITE_BASE || '/',
   plugins: [react()],
   css: {
     postcss: {
